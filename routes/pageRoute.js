f002773075a8be067fe992b5ca57be16c98b4ecc
hasanbekir97 <hasan.bekir.dogan@hotmail.com>
2022-10-08 02:35:37 +0300

    Front-End Completed

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,8 +1,11 @@
+const { application } = require("express");
 const express = require("express");
 const pageController = require("../controllers/pageController");
 
 const router = express.Router();
 
 router.route("/").get(pageController.getIndexPage);
+router.route("/login").get(pageController.getLoginPage);
+router.route('/register').get(pageController.getRegisterPage);
 
 module.exports = router;
